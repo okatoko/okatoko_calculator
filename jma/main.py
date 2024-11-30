e93ada8c500d1c035fecf8a5a6dac8a6b0fe55e8
okatoko <s2422040@stu.musashino-u.ac.jp>
2024-11-30 19:37:57 +0900

    上部の色を設定し、天気予報の文字を追加

--- jma/main.py
+++ jma/main.py
@@ -3,8 +3,25 @@ import flet as ft
 #背景色を#87CEFAに設定
 def main(page: ft.Page):
     page.bgcolor="#87CEFA"
+    page.horizontal_alignment = "stretch"
+    page.padding = 0
+    page.spacing = 0
 
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    #上部のバーを作成
+    header = ft.Container(
+        height=100,  # 高さを100に設定
+        bgcolor="#00008b",
+        padding=ft.padding.symmetric(horizontal=10),
+        content=ft.Row(
+            controls=[
+                ft.Text("天気予報", color="white", size=30),
+            ],
+        ),
+    )
 
+    # ヘッダーをフリー位置に配置し、ページ幅に広げる
+    page.add(ft.Row([
+        ft.Container(content=header, expand=True)
+    ]))
 
 ft.app(main)
