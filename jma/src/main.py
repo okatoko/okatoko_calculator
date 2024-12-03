8fb41016ea362b699a5d84b0d20ce74dd070f42b
okatoko <s2422040@stu.musashino-u.ac.jp>
2024-12-03 16:16:53 +0900

    背景色を設置し、上部のバーを作成

--- jma/src/main.py
+++ jma/src/main.py
@@ -1,25 +1,31 @@
 import flet as ft
+import requests
+import json
 
+# 気象庁APIのエンドポイントURL
+AREA_URL = "http://www.jma.go.jp/bosai/common/const/area.json"
+FORECAST_URL_TEMPLATE = "https://www.jma.go.jp/bosai/forecast/data/forecast/{}.json"
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    page.spacing = 0
+    page.padding = 0
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+     # 背景色を#87CEFAに設定
+    page.bgcolor = "#87CEFA"
+    page.horizontal_alignment = "stretch"
+    page.padding = 0
+    page.spacing = 0
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
-        )
+    # 上部のバーを作成
+    header = ft.Container(
+        height=100,
+        padding=ft.padding.symmetric(horizontal=15),
+        content=ft.Row(
+            controls=[
+                ft.Text("天気予報", color="white", size=35),
+            ],
+        ),
+        bgcolor="#4682B4",
     )
 
 
